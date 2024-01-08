29757a297fa2385e48af3526b746d2af555cf613
Lejla <leejlaa.73@gmail.com>
2024-01-08 18:57:55 +0100

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { lorem, random } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'typing';
+
+  randomText = lorem.sentence();
+  // firstNumber: number = 0;
+  enteredText = '';
+
+  // timer(){
+
+  //   while (this.randomText !== this.enteredText) {
+  //     setInterval( () => this.incrementNumber(this.firstNumber), 1000)
+
+  //   }
+
+  // }
+
+  // incrementNumber(num: number) {
+  //   this.firstNumber++;
+  // }
+
+  onInput(event: Event) {
+    this.enteredText = (event.target as HTMLInputElement).value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
+
